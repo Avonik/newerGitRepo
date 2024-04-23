ebb87db3ae970aff45b5fedfc658d1d813db13dd
Julian Hermes <julian@hermesfamilie.de>
2024-04-23 16:07:06 +0200

    added functionallity

--- venv/app.py
+++ venv/app.py
@@ -1,7 +1,28 @@
-from flask import Flask
+# backend.py
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return ("Hellow OWrld from flask ist geil nochmal test"git )
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_input', methods=['GET'])
+def get_input():
+    if request.method == 'GET':
+        input_value = request.args.get('input_value')
+        # Process input_value as needed
+        response = {'message': f'Received GET input: {input_value}'}
+        return jsonify(response)
+
+@app.route('/post_input', methods=['POST'])
+def post_input():
+    if request.method == 'POST':
+        data = request.get_json()
+        input_value = data['input_value']
+        # Process input_value as needed
+        response = {'message': f'Received POST input: {input_value}'}
+        return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
